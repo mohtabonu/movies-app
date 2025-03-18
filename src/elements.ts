3f9942594f2e83b38ab903c6a66251399d13a4c8
ShadowXX-08 <abduqodirovdavlatbek08@gmail.com>
2025-03-19 00:54:40 +0500

    bo'limlarni boshqarish funksiyasi va Registerni consolega chiqarish

--- src/elements.ts
+++ src/elements.ts
@@ -1,9 +1,9 @@
 export const mainContainer: HTMLDivElement = document.querySelector('#main-container')!;
 
 export const registerContainer: HTMLDivElement = document.querySelector('#register-container')!;
-export const regUsername: HTMLInputElement = document.querySelector('#reg-username')!;
-export const regPassword: HTMLInputElement = document.querySelector('#reg-password')!;
-export const regName: HTMLInputElement = document.querySelector('#reg-name')!;
+export const regUsername: HTMLInputElement = document.querySelector('#register-username')!;
+export const regPassword: HTMLInputElement = document.querySelector('#register-password')!;
+export const regName: HTMLInputElement = document.querySelector('#register-name')!;
 
 export const loginContainer: HTMLDivElement = document.querySelector('#login-container')!;
 export const loginUsername: HTMLInputElement = document.querySelector('#login-username')!;  
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-import { movies_information } from "./db";
+import { movies_information } from './db';
+import { mainContainer, registerContainer, regUsername, regPassword, regName, loginContainer, loginUsername, loginPassword } from './elements';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  function showContainers(targetContainer: HTMLDivElement) {
+    const containers = [mainContainer, registerContainer, loginContainer];
+
+    containers.forEach(container => {
+      container.style.display = container === targetContainer ? 'block' : 'none';
+    });
+  }
+  showContainers(mainContainer);
+  document.querySelector('button[onclick="showSection(\'main-container\')"]')?.addEventListener('click', () => showContainers(mainContainer));
+  document.querySelector('button[onclick="showSection(\'login-container\')"]')?.addEventListener('click', () => showContainers(loginContainer));
+  document.querySelector('button[onclick="showSection(\'register-container\')"]')?.addEventListener('click', () => showContainers(registerContainer));
+
+  const registerForm = document.querySelector('#register-container form')
+  if(registerForm){
+    registerForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+            console.log("Username:", regUsername.value);
+            console.log("Password:", regPassword.value);
+            console.log("Name:", regName.value);
+    })
+  }  
+});
